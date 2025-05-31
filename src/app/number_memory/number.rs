3a6863b6fdf7cac3b08c35d2a48baa28a83278b2
Taice <tai.stanic@gmail.com>
2025-06-01 00:50:57 +0200

    removing useless number thing

--- src/app/number_memory/number.rs
+++ src/app/number_memory/number.rs
@@ -3,51 +3,8 @@ use std::{
     ops::{Deref, DerefMut},
 };
 
-use ratatui::{
-    style::{Color, Style},
-    text::{Line, Span},
-};
-
 #[derive(Debug, Default, Clone, PartialEq, Eq)]
 pub struct Number(Vec<char>);
-
-impl Number {
-    pub fn get_styled_text(&self, other: &Self) -> Line {
-        if self.len() != other.len() {
-            panic!("something went wroong");
-        }
-
-        let mut text = Line::default();
-
-        for (i, &x) in self.iter().enumerate() {
-            if x == other[i] {
-                text += Span::raw(x.to_string());
-            } else {
-                text += Span::styled(x.to_string(), Style::default().bg(Color::Red));
-            }
-        }
-
-        text
-    }
-
-    pub fn get_wrong_styled_text(&self, other: &Self) -> Line {
-        if self.len() != other.len() {
-            panic!("something went wroong");
-        }
-
-        let mut text = Line::default();
-
-        for (i, &x) in self.iter().enumerate() {
-            if x == other[i] {
-                text += Span::styled(x.to_string(), Style::default().fg(Color::Green));
-            } else {
-                text += Span::styled(other[i].to_string(), Style::default().fg(Color::Red));
-            }
-        }
-
-        text
-    }
-}
 
 impl Deref for Number {
     type Target = Vec<char>;
